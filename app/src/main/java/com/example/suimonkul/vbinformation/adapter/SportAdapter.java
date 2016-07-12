e51f0c95e6dab920b32d5a873a675a75cd6fc12f
Suimonkul <suimonkul311200@gmail.com>
2016-07-12 20:25:26 +0600

    Deleted DetailActivity

---
+++ app/src/main/java/com/example/suimonkul/vbinformation/adapter/SportAdapter.java
@@ -0,0 +1,7 @@
+package com.example.suimonkul.vbinformation.adapter;
+
+/**
+ * Created by Suimonkul on 12.07.2016.
+ */
+public class SportAdapter {
+}
---
+++ app/src/main/java/com/example/suimonkul/vbinformation/fragment/SportFragment.java
@@ -0,0 +1,7 @@
+package com.example.suimonkul.vbinformation.fragment;
+
+/**
+ * Created by Suimonkul on 12.07.2016.
+ */
+public class SportFragment {
+}
